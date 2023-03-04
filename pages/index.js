83d5edaa137cce6b0c0c88d2a2213991dd299785
BrankoMili <105740275+BrankoMili@users.noreply.github.com>
2023-03-04 15:27:41 +0100

    first commit

---
+++ pages/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+import { client } from "../lib/client";
+import { Product, FooterBanner, HeroBanner } from "../components";
+import product from "@/sanity_ecommerce/schemas/product";
+
+const Home = ({ products, bannerData }) => {
+  return (
+    <>
+      <HeroBanner heroBanner={bannerData && bannerData[0]} />
+      <div className="products-heading">
+        <h2>Best Selling Product</h2>
+        <p>Speakers on many variations</p>
+      </div>
+      <div className="products-container">
+        {products?.map((product) => (
+          <Product key={product._id} product={product} />
+        ))}
+      </div>
+
+      <FooterBanner footerBanner={bannerData && bannerData[0]} />
+    </>
+  );
+};
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData },
+  };
+};
+
+export default Home;
